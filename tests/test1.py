ad73c3c716fec699cc844810d0e8aea27caa2280
Douglas Bagnall <douglas@paradise.net.nz>
2009-11-04 00:14:07 +1300

    adding a test file, and the old "compiler" based pyserf

---
+++ tests/test1.py
@@ -0,0 +1,29 @@
+"Module documentation"
+
+def foo(int_a, int_b):
+    "function documentation"
+    return(int_c, int_b)
+
+class bar:
+    "class documentation"
+    def baz(self, int_c):
+        "method documentation"
+        print "line 1 to skip"
+        f("c code  to skip")
+        print "line 2 to skip"
+        return int_c
+
+    def foo(self):
+        """this method should not clash with the function"""
+
+    def hi(a):
+        "should raise a warning"'about first argument not being \\\"self\\\"'
+
+    def __init__(self):
+        print "hello"
+
+    def quux(self):
+        return None
+
+def self_test():
+    return int_a
